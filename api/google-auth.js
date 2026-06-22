3dcf5a508dab22a800aaa839a3465cc773a0a4ab
karenramirez999-a11y <karenramirez999@gmail.com>
2026-06-22 14:39:52 -0500

    google-auth.js

--- api/google-auth.js
+++ api/google-auth.js
@@ -1,6 +1,15 @@
 const { google } = require('googleapis');
 
 module.exports = async function handler(req, res) {
+  const { code } = req.query;
+
+  if (code) {
+    return res.status(200).json({
+      ok: true,
+      code
+    });
+  }
+
   const oauth2Client = new google.auth.OAuth2(
     process.env.GOOGLE_CLIENT_ID,
     process.env.GOOGLE_CLIENT_SECRET,
